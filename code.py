5276fac9e009bdb3b00971cfaef84c708c12b9f6
Soerenna Farhoudi <72605232+shwiftyboy@users.noreply.github.com>
2021-03-05 13:10:35 +0100

    Update code.py
    more efficient version of max_keys list and set it to 10

--- code.py
+++ code.py
@@ -11,22 +11,11 @@ def find_word(word):
         return 'Not Found'
 
 test = 'braim' # a string to test the code
-matches = dict() # create a dictionary which contains all the with their respective ratios if the ratio > 0.5
+matches = dict() # create a dictionary which contains keys with their respective ratios if the ratio > 0.5
 for key, value in data.items():
     s = SequenceMatcher(a=key,b=test)
     ratio = s.ratio()
     if ratio > 0.5:
         matches[key] = ratio
 
-max_ratios = sorted(matches.values())[-5:] # 5 highest similarity ratios
-max_ratios = list(set(max_ratios)) # remove duplicates
-
-# two possible ways of producing a list with the keys that correspond to the five highest similarity ratios
-max_keys = [key for key, value in matches.items() if value in max_ratios] # an unsorted list of keys with 
-max_keys = [] # a sorted list of keys in descending order, not efficient though will try to optimise if I go with this one.
-for ratio in max_ratios[::-1]:
-    for key, value in matches.items():
-        if value == ratio:
-            max_keys.append(key)
-print(max_keys)
-
+max_keys = sorted(matches.items(), key=lambda x: x[1], reverse=True)[:10] # timed the code and this third version came out 10 times faster then the previous ones on average
